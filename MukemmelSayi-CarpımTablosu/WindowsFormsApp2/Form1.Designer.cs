37d72cc28467bab916880ac1e7b12be925eb6af1
AlperenCanY <168603289+AlperenCanY@users.noreply.github.com>
2025-02-19 20:24:46 +0300

    Add files via upload

---
+++ MukemmelSayi-CarpımTablosu/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lstCarpimTablosu = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sorgula";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.MistyRose;
+            this.groupBox1.Controls.Add(this.lblSonuc);
+            this.groupBox1.Controls.Add(this.txtSayi);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(245, 282);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mükemmel Sayı Bulma";
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(50, 45);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(100, 22);
+            this.txtSayi.TabIndex = 1;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(10, 144);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(0, 16);
+            this.lblSonuc.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.RosyBrown;
+            this.groupBox2.Controls.Add(this.lstCarpimTablosu);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(254, 54);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(245, 282);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Çarpım Tablosu";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Yazdır";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lstCarpimTablosu
+            // 
+            this.lstCarpimTablosu.FormattingEnabled = true;
+            this.lstCarpimTablosu.ItemHeight = 16;
+            this.lstCarpimTablosu.Location = new System.Drawing.Point(18, 35);
+            this.lstCarpimTablosu.Name = "lstCarpimTablosu";
+            this.lstCarpimTablosu.Size = new System.Drawing.Size(120, 84);
+            this.lstCarpimTablosu.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(521, 368);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Sistemler";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lstCarpimTablosu;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MukemmelSayi-CarpımTablosu/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int sayi = Convert.ToInt32(txtSayi.Text);
+
+                int toplam = 0;
+
+                for (int i = 1; i < sayi; i++)
+                {
+                    if (sayi % i == 0)
+                    {
+                        toplam += i;
+                    }
+                }
+
+                if (toplam == sayi)
+                {
+                    lblSonuc.Text = sayi + " Mükemmel bir sayıdır.";
+                }
+                else
+                {
+                    lblSonuc.Text = sayi + " Mükemmel bir sayı değildir.";
+                }
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Sayı Hatalı");
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            groupBox2.BackColor = Color.Green;
+            lstCarpimTablosu.Items.Clear();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    int carpim = i * j;
+                    lstCarpimTablosu.Items.Add($"{i} x {j} = {carpim}");
+                }
+            }
+        }
+    }
+}
